d48cdafd7e7f3b6f09a16d3a5c36eccc4ca0c0eb
Brenton Jackson <bjavonjackson@gmail.com>
2022-04-02 15:23:06 -0400

    send frames to web app

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Frame from "../Frame/Frame";
+
+const Home = () => {
+  return (
+    <div className="home-div">
+      <Frame />
+    </div>
+  );
+};
+
+export default Home;
