f0ee0e2c9cc4be2e5900239a74075638c79a519b
Brenton Jackson <bjavonjackson@gmail.com>
2022-04-03 03:54:34 -0400

    add neural setup code

--- server/server.py
+++ server/server.py
@@ -10,6 +10,34 @@ imageHub = imagezmq.ImageHub()
 # initialize neural network stuff #
 
 
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1]
+                     for i in net.getUnconnectedOutLayers()]
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = str(classes[class_id])
+    color = COLORS[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x-10, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+classes = None
+with open('ml_classes.txt', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+net = cv2.dnn.readNet('ml.weights', 'ml.cfg')
+
+class_ids = []
+confidences = []
+boxes = []
+conf_threshold = 0.9
+nms_threshold = 0.4
 ###################################
 
 
